cf2f17be5a52490fba896786faa05170869bfff4
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-09-25 01:26:40 -0300

    feat: add hire component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@ import { FloatingButton } from "@/components/marketing/floating-button";
 import { Footer } from "@/components/marketing/footer";
 import { Header } from "@/components/marketing/header";
 import { Hero } from "@/components/marketing/hero";
+import { Hire } from "@/components/marketing/hire";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <main className="bg-white">
+        {/* Seção Hero */}
         <Hero />
+        {/* O que você pode contratar */}
+        <Hire />
         <Content />
       </main>
       <Footer />
